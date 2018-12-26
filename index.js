63227c21720914acba0c267c9a48966f35fdede3
Xavier Johnson <letitbexavi@outlook.com>
2018-12-26 01:35:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // add solution here
+
+  function theBeatlesPlay(musicians, instruments) {
+    var emptyArr = [];
+
+    for (var i=0; i < musicians.length; i++){
+      emptyArr.push(musicians[i]+ ' plays ' + instruments[i]);
+      }
+      return emptyArr;
+    }
+
+    function johnLennonFacts(facts) {
+      var i = 0;
+      while (facts.length < 5){
+        facts.push(facts[i] + '!!!');
+        facts++;
+      }
+        return facts;
+    }
